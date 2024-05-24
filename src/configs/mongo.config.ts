75d621231a3fd6632d2e832a87fe61c5e99b30bb
nicmora <nicmora24@gmail.com>
2024-05-24 15:05:12 -0300

    fix docker compose

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -5,7 +5,9 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
 
 (async (): Promise<void> => {
   await mongoose
-    .connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`)
+    .connect(
+      `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`
+    )
     .then((db) => {
       console.log("Database is connected to: ", db.connection.name);
     })
